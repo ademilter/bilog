582050b5128f6987170cddf26cf14d6d485cd33a
Adem ilter <ademilter@gmail.com>
2022-03-05 13:30:50 +0300

    lanet olsun

--- pages/[username]/[slug].tsx
+++ pages/[username]/[slug].tsx
@@ -6,117 +6,126 @@ import type { UserProfile } from "@auth0/nextjs-auth0";
 
 import { markdownToHtml } from "lib/editor";
 import prisma from "lib/prisma";
-import { deepCopy } from "lib/helper";
+import GlobalStoreContext from "context/global";
 
 import Layout from "components/Layout";
 import { PostProps } from "components/Post";
 import Button from "components/Button";
+import { deepCopy } from "../../lib/helper";
 
 // Examples
 // https://lux.camera/halide-2-7-its-a-keeper/
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const { slug } = params;
-  const id = slug.toString().split("-").at(-1);
-  let post = null;
-  let error = null;
+  const id = `${slug}`.toString().split("-").at(-1);
 
-  // await prisma.post
-  //   .findUnique({
-  //     where: { id },
-  //   })
-  //   .then((data) => {
-  //     if (!data) {
-  //       error = "Post not found";
-  //     }
-  //     post = data;
-  //   })
-  //   .catch((e) => {
-  //     error = e;
-  //   });
+  const post = await prisma.post.findUnique({
+    where: { id },
+    select: {
+      id: true,
+      title: true,
+      content: true,
+      slug: true,
+      published: true,
+      createdAt: true,
+      tags: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
+      likes: {
+        select: {
+          id: true,
+        },
+      },
+      user: {
+        select: {
+          id: true,
+          username: true,
+          name: true,
+          picture: true,
+        },
+      },
+    },
+  });
 
   return {
-    props: {
-      slug,
-      id,
-    },
+    props: { post: deepCopy(post) },
   };
 };
 
-const Post: React.FC<{
-  user: UserProfile;
-  slug: unknown;
-  id: unknown;
-}> = ({ user: session, slug, id }) => {
-  console.log(session);
-  console.log(id);
-  console.log(slug);
+const Post: React.FC<{ user: UserProfile; post: PostProps }> = ({
+  user: sessionUser,
+  post,
+}) => {
+  const { id, title, content, published, user } = post;
 
-  // const { id, title, content, published, user } = post;
-  //
-  // const postBelongsToUser = session && session.nickname === user.username;
-  //
-  // async function publishPost(id: string) {
-  //   await fetch(`/api/publish/${id}`, {
-  //     method: "PUT",
-  //   });
-  //   return Router.push("/");
-  // }
-  //
-  // async function deletePost(id: string) {
-  //   await fetch(`/api/post/${id}`, {
-  //     method: "DELETE",
-  //   });
-  //   return Router.push("/");
-  // }
+  const postBelongsToUser =
+    sessionUser && sessionUser.nickname === user.username;
+
+  async function publishPost(id: string) {
+    await fetch(`/api/publish/${id}`, {
+      method: "PUT",
+    });
+    return Router.push("/");
+  }
+
+  async function deletePost(id: string) {
+    await fetch(`/api/post/${id}`, {
+      method: "DELETE",
+    });
+    return Router.push("/");
+  }
 
   return (
     <Layout>
-      {/*<div className="mt-4 space-x-2">*/}
-      {/*  {postBelongsToUser && (*/}
-      {/*    <>*/}
-      {/*      {published ? (*/}
-      {/*        <Button*/}
-      {/*          className="!bg-blue-100 text-blue-700"*/}
-      {/*          onClick={() => Router.push("/edit/[id]", `/edit/${id}`)}*/}
-      {/*        >*/}
-      {/*          Edit*/}
-      {/*        </Button>*/}
-      {/*      ) : (*/}
-      {/*        <>*/}
-      {/*          <Button*/}
-      {/*            className="!bg-blue-100 text-blue-700"*/}
-      {/*            onClick={() => publishPost(id)}*/}
-      {/*          >*/}
-      {/*            Publish*/}
-      {/*          </Button>*/}
-      {/*          <Button*/}
-      {/*            className="!bg-blue-100 text-blue-700"*/}
-      {/*            onClick={() => Router.push("/draft/[id]", `/draft/${id}`)}*/}
-      {/*          >*/}
-      {/*            Edit*/}
-      {/*          </Button>*/}
-      {/*        </>*/}
-      {/*      )}*/}
-      {/*      <Button*/}
-      {/*        className="!bg-red-100 text-red-700"*/}
-      {/*        onClick={() => deletePost(id)}*/}
-      {/*      >*/}
-      {/*        Delete*/}
-      {/*      </Button>*/}
-      {/*    </>*/}
-      {/*  )}*/}
-      {/*</div>*/}
+      <div className="mt-4 space-x-2">
+        {postBelongsToUser && (
+          <>
+            {published ? (
+              <Button
+                className="!bg-blue-100 text-blue-700"
+                onClick={() => Router.push("/edit/[id]", `/edit/${id}`)}
+              >
+                Edit
+              </Button>
+            ) : (
+              <>
+                <Button
+                  className="!bg-blue-100 text-blue-700"
+                  onClick={() => publishPost(id)}
+                >
+                  Publish
+                </Button>
+                <Button
+                  className="!bg-blue-100 text-blue-700"
+                  onClick={() => Router.push("/draft/[id]", `/draft/${id}`)}
+                >
+                  Edit
+                </Button>
+              </>
+            )}
+            <Button
+              className="!bg-red-100 text-red-700"
+              onClick={() => deletePost(id)}
+            >
+              Delete
+            </Button>
+          </>
+        )}
+      </div>
 
-      {/*<div className="mt-10 prose prose-zinc max-w-none">*/}
-      {/*  {!published && <div>(Draft)</div>}*/}
+      <div className="mt-10 prose prose-zinc max-w-none">
+        {!published && <div>(Draft)</div>}
 
-      {/*  <p>{user.name}</p>*/}
+        <p>{user.name}</p>
 
-      {/*  <div className="text-5xl leading-none font-bold">{title}</div>*/}
+        <div className="text-5xl leading-none font-bold">{title}</div>
 
-      {/*  <div dangerouslySetInnerHTML={{ __html: markdownToHtml(content) }} />*/}
-      {/*</div>*/}
+        <div dangerouslySetInnerHTML={{ __html: markdownToHtml(content) }} />
+      </div>
     </Layout>
   );
 };
